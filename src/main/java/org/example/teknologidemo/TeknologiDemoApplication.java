70feaaf7f84d17ce7bcc785a27f56ba1366231e5
amaliekea <amje0001@stud.kea.dk>
2025-04-17 14:03:39 +0200

    initial

---
+++ src/main/java/org/example/teknologidemo/TeknologiDemoApplication.java
@@ -0,0 +1,13 @@
+package org.example.teknologidemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeknologiDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeknologiDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/teknologidemo/TeknologiDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.teknologidemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeknologiDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

8d11661e8e3307c18304e3428b70273e9a493542
amaliekea <amje0001@stud.kea.dk>
2025-04-17 14:14:47 +0200

    another commit

--- src/main/java/org/example/teknologidemo/controllers/HelloApplication.java
+++ src/main/java/org/example/teknologidemo/controllers/HelloApplication.java
@@ -4,7 +4,7 @@ package org.example.teknologidemo.controllers;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
+//hej
 @SpringBootApplication
 @RestController
 public class HelloApplication {
